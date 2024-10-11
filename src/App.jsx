844ef554a3a424a659558080175782361bc705dc
Fernanda <fbp@ecomp.poli.br>
2024-10-10 21:33:08 -0300

    fix: add /notFound path

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/notFound" element={<NotFound />} />
         <Route path="/*" element={<NotFound />} />
       </Routes>
     </Router>
